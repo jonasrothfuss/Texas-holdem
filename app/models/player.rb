a70f1423d68a4333663b7578f39c5330c10f9f5a
jonasrothfuss <jonas.rothfuss@gmx.de>
2015-10-24 01:31:00 +0000

    added player model

---
+++ app/models/player.rb
@@ -0,0 +1,54 @@
+class Player
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  has_one user
+  
+  field :buy_in, type: In
+  field :chip_amount, type: Int
+  
+  def self.new_player (user, buy_in)
+    unless buy_in_ok?(user, buy_in)
+      self.create({user: user, buy_in: buy_in, chip_amount: buy_in})
+    else  
+      raise BuyInError, 'buy_in amounth exceeds users balance'
+    end
+  end
+    
+    
+  def buy_in_ok?(user, buy_in) 
+    return user.balance >= buy_in
+  end
+  
+  
+  def win(amounth)
+    chip_amounth += amounth
+    user.balance += amounth
+    user.save!
+  end
+  
+  
+  def bet(amounth)
+    unless bet_ok?(amounth)
+      chip_amounth -= amounth
+      user.balance -= amounth
+      user.save!
+    else
+      raise InvalidBetError, 'bet exceeds users chip_amounth'
+    end
+  end
+  
+  
+  def bet_ok?(amounth)
+    return amounth <= user.chip_amount
+  end
+  
+end
+
+
+class InvalidBetError < StandardError
+end
+
+
+class BuyInError < StandardError
+end
