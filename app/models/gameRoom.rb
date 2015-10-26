7f706d092c5eb6683884d2223f99076d495ced90
jonasrothfuss <jonas.rothfuss@gmx.de>
2015-10-26 23:16:06 +0000

    bugfixes

---
+++ app/models/gameRoom.rb
@@ -0,0 +1,48 @@
+class GameRoom
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  
+  has_and_belongs_to_many :players
+  has_many :rounds
+  
+  
+  field :name, type: String
+  field :max_players, type: Integer
+  field :limit, type: Integer
+  field :active, type: Boolean
+  field :isPrivate, type: Boolean
+  
+  def removePlayer(player)
+    self.players.delete(player)
+    self.player.destroy
+  end
+  
+  
+  def close_room
+    self.active = false
+  end
+  
+  
+  def newRound
+    rounds << Round.newRound(players)
+  end
+  
+  
+  def addPlayer(user, buy_in)
+    unless buyInOk?(buy_in)
+      raise BuyInExceedsLimitError
+    else
+      self.players << Player.newPlayer(user, buy_in)
+    end
+  end
+  
+  
+  def buyInOk?(buy_in)
+    return buy_in <= self.limit
+  end
+  
+end
+
+
+class BuyInExceedsLimitError < StandardError
+end
