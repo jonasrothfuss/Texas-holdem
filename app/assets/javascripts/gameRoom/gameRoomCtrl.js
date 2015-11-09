aa54ac056ec7df7b8974f11d403e62c14f6e0eb8
dilshanraja <dilshan.raja@gmail.com>
2015-11-09 01:24:24 -0800

    GameRoom Initial Functionality - Frontend

--- app/assets/javascripts/gameRoom/gameRoomCtrl.js
+++ app/assets/javascripts/gameRoom/gameRoomCtrl.js
@@ -1,4 +1,22 @@
-pokerApp.controller('gameRoomCtrl', ['$scope', function GameRoomCtrl($scope){
-	
-	
+pokerApp.controller('gameRoomCtrl', ['$scope', '$rootScope', '$stateParams', 'apiServices', 'Pusher', function ($scope, $rootScope, $stateParams, apiServices, Pusher){
+	$scope.gameRoom = {};
+	$scope.sending = false;
+	$scope.messages = [];
+	$scope.message = '';
+
+	joinAndLoad();
+
+	//--Pusher Subscriptions--
+	Pusher.subscribe('gameroom-' + $stateParams.gameId, 'newplayer', function(player){
+		console.log("new player");
+		$scope.gameRoom.players.push(player);
+	});
+
+	//--Private Funcs--
+	function joinAndLoad(){
+		apiServices.GameService.Join($stateParams.gameId, {user: $rootScope.user}).success(function (result){
+			$scope.gameRoom = result;
+		});
+	}
+
 }]);
