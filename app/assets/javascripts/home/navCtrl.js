6c7f69c5fab1325aaaf7476eefb04998a2daec88
dilshanraja <dilshan.raja@gmail.com>
2015-11-09 01:21:59 -0800

    Angular Auth refactor

--- app/assets/javascripts/home/navCtrl.js
+++ app/assets/javascripts/home/navCtrl.js
@@ -1,23 +1,4 @@
 'use strict';
 
-pokerApp.controller('NavCtrl', ['$scope', '$state', 'Auth', function($scope, $state, Auth){
-	$scope.signedIn = Auth.isAuthenticated;
-	$scope.logout = Auth.logout;
-
-	Auth.currentUser().then(function (user){
-		$scope.user = user;
-	});
-
-	$scope.$on('device:new-registration', function (e, user){
-		$scope.user = user;
-	});
-
-	$scope.$on('devise:login', function(e, user){
-		$scope.user = user;
-	});
-
-	$scope.$on('devise:logout', function(e, user){
-		$scope.user = {};
-		$state.go('login');
-	});
+pokerApp.controller('NavCtrl', ['$scope', '$rootScope', '$state', function($scope, $rootScope, $state){
 }]);
