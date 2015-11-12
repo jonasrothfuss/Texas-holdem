3f2e577028cc1806f5aa206af277090fc832e500
dilshanraja <dilshan.raja@gmail.com>
2015-11-12 00:54:20 -0800

    AngularJS isEmpty Filter

---
+++ app/assets/javascripts/filters.js
@@ -0,0 +1,12 @@
+pokerApp
+	.filter('isEmpty', function () {
+		var bar;
+		return function (obj) {
+			for (bar in obj) {
+				if (obj.hasOwnProperty(bar)) {
+					return false;
+				}
+			}
+			return true;
+		};
+	});
