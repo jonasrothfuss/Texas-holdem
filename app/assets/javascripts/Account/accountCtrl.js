af200bcbacd2df97ab0b374c70bcf61929559724
jonasrothfuss <jonas.rothfuss@gmx.de>
2015-11-14 06:12:57 +0000

    simple account controller

--- app/assets/javascripts/Account/accountCtrl.js
+++ app/assets/javascripts/Account/accountCtrl.js
@@ -1,1 +1,5 @@
-pokerApp.controller('accountCtrl')
+'use strict';
+
+pokerApp.controller('accountCtrl', ['$scope', '$rootScope', '$state', '$timeout', 'Auth', function($scope, $rootScope, $state){
+}]);
+
