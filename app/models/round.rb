8e85581f825b306c52bcd1b90064990ad4a5d0de
dilshanraja <dilshan.raja@gmail.com>
2015-11-13 19:27:41 -0800

    Revert "Round Stage Finished Check - Bug Fix"
    This reverts commit 3a2abf54a53f7434ca63187c799cd0e2ea89b8db.

--- app/models/round.rb
+++ app/models/round.rb
@@ -168,13 +168,14 @@ class Round
 
   def stage_finished?
     bet = self.hands[0].bet
+    action = self.hands[0].action_count
 
     self.hands.each do |h|
       if bet != h.bet
         return false
       end
 
-      if h.action_count == 0
+      if action != h.action_count
         return false
       end
     end
