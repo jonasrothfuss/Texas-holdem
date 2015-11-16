3b28d54ba9ce3454bd56ea900c72761df58c7879
jonasrothfuss <jonas.rothfuss@gmx.de>
2015-11-16 03:01:12 +0000

    Post Routes for Editing and Deleting Account

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  post '/editaccount', to: 'account#edit'
+  post '/deleteaccount', to: 'account#delete'
+  
   root to: 'application#angular'
 
   post 'pusher/auth'
