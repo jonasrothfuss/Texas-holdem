ef718d685143aeff93258ed84d4975648951657b
jonasrothfuss <jonas.rothfuss@gmx.de>
2015-11-26 04:00:03 +0000

    callback route for facebook login

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
+  
+  get '/users/sign_up', to: 'application#angular'
+  
   post '/editaccount', to: 'account#edit'
   post '/deleteaccount', to: 'account#delete'
   
