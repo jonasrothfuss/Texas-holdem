a5bcf8424563a01578c5509bb8d7b384ffe2a939
jonasrothfuss <jonas.rothfuss@gmx.de>
2015-11-26 08:31:53 +0000

    hide password inputs in the edit account section when user is logged in via facebook

--- app/assets/javascripts/account/accountCtrl.js
+++ app/assets/javascripts/account/accountCtrl.js
@@ -3,6 +3,18 @@
 pokerApp.controller('accountCtrl', ['$scope', '$rootScope', '$state', '$http', function ($scope, $rootScope, $state, $http) {
 	$scope.success = false;
 	$scope.showEditor = false;
+	
+	/*
+	$(document).ready(function(){
+	  
+	  //hide passowrd filds for users logged in with fb
+	  if ($scope.user.uid.length > 4){
+      $("#new_password_input").hide()
+    $("#email_input").prop("disabled", true)
+    }
+	   
+	 })
+	 */
 
 	$scope.save = function() {
 		$scope.error = {};
